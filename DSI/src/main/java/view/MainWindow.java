72d44a6102ef1805a8a79b350417a7cfe4b73cb5
mcadrian <mc.adrian.g@gmail.com>
2014-04-03 20:08:22 +0200

    Segundo commit

--- DSI/src/main/java/view/MainWindow.java
+++ DSI/src/main/java/view/MainWindow.java
@@ -1,10 +1,5 @@
 package view;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import controller.Engine;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
